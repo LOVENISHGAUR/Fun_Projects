87e349b5f67e7161b946f67ca6c3582ac54b336e
Lovenish Gaur <lovenishgaur9@gmail.com>
2020-12-13 22:47:06 +0530

    sample madlibs

---
+++ repository_madlibs/hero.py
@@ -0,0 +1,14 @@
+def madlib():
+    verb1 = input("Verb: ")
+    verb2 = input("Verb: ")
+    verb3 = input("Verb: ")
+    adj1 = input("Adjective: ")
+    noun1 = input("Noun: ")
+    noun2 = input("Noun: ")
+    famous_person1 = input("Famous person: ")
+    famous_person2 = input("Famous person: ")
+    famous_person3 = input("Famous person: ")
+
+    madlib = f"Why do we fall? So we can learn to {verb1} ourselves back up – {famous_person1} /You can take {verb2} my suits, you can take {verb2} my home,but there’s one thing you can never take {verb2} from me. I am {famous_person2} / “Doesn’t matter what the press says. Doesn’t matter what the politicians or the mobs say. Doesn’t matter if the whole {noun1} decides that something wrong is something right. This nation was {adj1} on one principle above all else: the requirement that we stand up for what we {verb3}, no matter the odds or the consequences.When the mob and the press and the world tell you to move, your job is to plant yourself like a tree beside the {noun2} of truth,and tell the whole world – “No, you move.”” – {famous_person3}"
+
+    print(madlib)
